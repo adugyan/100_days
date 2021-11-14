610ab63b52bdb424fbcc02082703707453d12a58
Kofi Adu-Gyan <44382721+adugyan@users.noreply.github.com>
2021-11-14 17:06:52 -0600

    Create caeser_cipher.py

---
+++ Caesar_cipher/caeser_cipher.py
@@ -0,0 +1,44 @@
+from replit import clear
+from art import logo
+
+logo: str # ASCII art logo for the game
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text: str = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+def play_again() -> None:
+  player_choice = input("\nWould you like to record another message? Enter 'y' or 'n': ").lower()
+
+  if player_choice == 'y':
+    clear()
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    play_again()
+  elif player_choice == 'n':
+    print("Goodbye!")
+  else:
+    print("That wasn\'t a valid option")
+
+print(logo)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+shift = shift % 26 # Note if you want to visit a range within a constraint % by the max number
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+play_again()
