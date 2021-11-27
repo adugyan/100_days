8d941bcac04d04a3a0ff49270cd8153f36d6f652
Kofi Adu-Gyan <44382721+adugyan@users.noreply.github.com>
2021-11-27 11:00:06 -0600

    Update main.py

--- Higher_lower/main.py
+++ Higher_lower/main.py
@@ -5,38 +5,38 @@ from game_data import data
 
 print(logo)
 
+def format_name(option):
+  """
+  Takes the option and returns a string to be read.
+  """
+  option_name = option['name']
+  option_description = option['description']
+  option_country = option['country']
+  option_count = option['follower_count']
+  return f"{option_name}, a {option_description}, from {option_country}. Hint :{option_count}"
+  
+
 def game_loop():
   """
   Pack the follower_count into variables
   a. compare the selection to both of the variables 
-  
   """
   winner: int # will contain the higher follower count
   score: int = 0 # records how many round the player has won
   end_game: bool = False
   option_A: int # will contain the value for A
-  option_B: int # containes the value for option B
+  option_B: int # contains the value for option B
 
   while end_game == False:
     both_follower_count: list = [] # empty list that both of the follower counts will be appended into so that the highest one can be extracted for a later step
     option_A = data[random.randrange(len(data))]
     option_B = data[random.randrange(len(data))]
-
-    # format the values for A
-    A_name = option_A['name']
-    A_description = option_A['description']
-    A_country = option_A['country']
     A_count = option_A['follower_count']
-
-    # format the values for B
-    B_name = option_B['name']
-    B_description = option_B['description']
-    B_country = option_B['country']
     B_count = option_B['follower_count']
 
-    print(f"Compare A: {A_name}, a {A_description}, from {A_country}. Hint : {A_count}")
+    print(f"Compare A: {format_name(option_A)}")
     print(vs)
-    print(f"Against B: {B_name}, a {B_description}, from {B_country}. Hint : {B_count}")
+    print(f"Against B: {format_name(option_B)}")
 
 
     both_follower_count.append(A_count)
