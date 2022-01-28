7aacbb5784d43d92746e539855a054d2f2899d09
adugyan <adugyan1602@gmail.com>
2022-01-27 22:29:25 -0600

    added forward movement

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,14 +1,15 @@
 from turtle import Screen, Turtle
-from typing import List, Tuple, Any
+from typing import List, Tuple
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title('My snake Game')
+screen.title('My Snake Game')
+screen.tracer(0)  # pauses screen
 
 starting_positions: List[Tuple] = [(0, 0), (-20, 0), (-40, 0)]
 segments: list = []
-game_on: bool = True
 
 for position in starting_positions:
     snake_segment = Turtle('square')
@@ -19,9 +20,17 @@ for position in starting_positions:
 
 
 def snake_movement(segments):
-    for seg in segments:
-        seg.forward(20)
+    screen.tracer(True)
+    time.sleep(0.1)
+    # Reminder the variable integer becomes the [index_key] when range is in play
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
+
+game_on: bool = True
 while game_on:
     snake_movement(segments)
 
