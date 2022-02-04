6e7f70d1e81ea26b02677a346fc3ea2949fe3881
Adu-Gyan Kofi <Adu-Gyan_Kofi@shure.com>
2022-02-03 21:31:48 -0600

    added move functions and ball class

--- Pong/main.py
+++ Pong/main.py
@@ -1,6 +1,8 @@
+import time
 from turtle import Screen
 from scoreboard import ScoreBoard
 from paddle import Paddle
+from ball import Ball
 
 
 screen = Screen()
@@ -9,15 +11,21 @@ screen.bgcolor("black")
 screen.title('Pong')
 screen.tracer(0)  # pauses screen
 
-p1_paddle = Paddle([(-350, 0)])
+p1_paddle = Paddle((-350, 0))
+p2_paddle = Paddle((350, 0))
+ball = Ball()
 scoreboard = ScoreBoard()
 
-# screen.onkey(key='Up', fun=paddle.move_up)
-#screen.onkey(key='Down', fun=snake.move_down)
+screen.onkey(key='w', fun=p1_paddle.move_up)
+screen.onkey(key='s', fun=p1_paddle.move_down)
+screen.onkey(key='Up', fun=p2_paddle.move_up)
+screen.onkey(key='Down', fun=p2_paddle.move_down)
 screen.listen()
 
 game_on: bool = True
 while game_on:
+    time.sleep((0.1))
     screen.update()
+    ball.move()
 
 screen.exitonclick()
--- Pong/paddle.py
+++ Pong/paddle.py
@@ -1,4 +1,4 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 P1_POSITION: dict = {'x_pos': -350, 'y_pos': 0}
 P2_POSITION: dict = {'x_pos': 350, 'y_pos': 0}
@@ -6,20 +6,19 @@ DIRECTIONS: dict = {'Up': 90, 'Down': 270}
 
 class Paddle(Turtle):
     def __init__(self, position):
-        super().__init__
-        self.shape('square')
-        self.color('white')
+        super().__init__()
+        self.shape("square")
+        self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
 
-
     def move_up(self):
-        new_y = self.paddle() + 20
+        new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
     def move_down(self):
-        new_y = self.paddle() + 20
+        new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
 
             
