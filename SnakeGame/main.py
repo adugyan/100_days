af6f81e5b04f6e197801690281203174ce43d883
Adu-Gyan Kofi <Adu-Gyan_Kofi@shure.com>
2022-02-08 21:20:49 -0600

    Added highscore functionality to the snake game

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -34,13 +34,15 @@ while game_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+        #game_on = False
 
     #Detect collision with tail
     for segment in snake.segments[1:]: #everything after [0]
         if snake.head.distance(segment) < 10:
-            game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+            #game_on = False
 
 screen.exitonclick()
--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -4,6 +4,8 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
+        with open('high_score.txt') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.score = 0
         self.penup()
@@ -11,14 +13,20 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
         self.hideturtle()
 
+
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 20, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 20, "normal"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('high_score.txt', mode='w') as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align="center", font=("Courier", 20, "normal"))
