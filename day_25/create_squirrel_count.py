57ba9e80950c8bb838c71263621fd955f98c86e3
Adu-Gyan Kofi <Adu-Gyan_Kofi@shure.com>
2022-02-14 22:36:24 -0600

    Finish census code

--- day_25/create_squirrel_count.py
+++ day_25/create_squirrel_count.py
@@ -1,9 +1,21 @@
 import pandas
 
-# TODO find a way search the census by color
+census_data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+grey_counter: int = 0
+red_counter: int = 0
+black_counter: int = 0
 
-# TODO increase the counter for everytime that color pops up
+for data in census_data['Primary Fur Color']:
+    if data == 'Gray':
+        grey_counter += 1
+    elif data == 'Cinnamon':
+        red_counter += 1
+    elif data == "Black":
+        black_counter += 1
 
-# TODO create a dictionary with this information
-
-# TODO use the pandas method to make this a CSV file
+color_dict: dict = {
+    'Fur Color': ['Gray', 'Cinnamon', 'Black'],
+    'Count': [grey_counter, red_counter, black_counter]
+}
+color_data_frame = pandas.DataFrame(color_dict)
+color_data_frame.to_csv('color_count.csv')
