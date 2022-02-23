0c95b849547047ebcc4320bbff7f82cdafdbf250
adugyan <adugyan1602@gmail.com>
2022-02-23 09:50:44 -0600

    finished states game

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import pandas
 
 screen = Screen()
 turtle = Turtle()
@@ -8,6 +9,34 @@ screen.addshape(image)
 
 turtle.shape(image)
 
+data = pandas.read_csv('50_states.csv')
+all_states = data['state'].to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title='Guess the State', prompt="What's another state's name?")
-print(answer_state)
+
+def place_name():
+    t = Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data['state'] == answer_state]
+    t.goto(int(state_data.x), int(state_data.y))
+    t.pendown()
+    t.write(answer_state)
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}",
+                                    prompt="What's another state's name?").title()
+    guessed_states.append(answer_state)
+    if answer_state in all_states:
+        place_name()
+    elif 'Exit' in guessed_states:
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv('state_to_learn')
+
+
+
