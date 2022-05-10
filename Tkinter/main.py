17c5d6714407e8da59f8d529e3b251af5dd94c98
adugyan <adugyan1602@gmail.com>
2022-05-09 21:58:12 -0500

    widget demo initital commit

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -1,13 +1,31 @@
-import tkinter
+from tkinter import Tk, Label, Button, Entry
 import turtle
 
-window = tkinter.Tk()
+window = Tk()
 window.title('My first GUI Program')
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+# Label
+
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
-tim = turtle.Turtle()
-tim.write()
+my_label.config(text="New Text")
+
+# Button
+
+def button_clicked():
+    print("I got clicked")
+    new_label = input.get()
+    my_label.config(text=new_label)
+
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
+
 window.mainloop()
