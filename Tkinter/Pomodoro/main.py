38cdf9ae4e4433033dc0647eaa766bab8208acb5
adugyan <adugyan1602@gmail.com>
2022-05-24 23:36:41 -0500

    Added start timer functionality

--- Tkinter/Pomodoro/main.py
+++ Tkinter/Pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from math import floor
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,8 +14,21 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count: int):
+    count_min = floor(count / 60)
+    count_sec = count % 60
+    if count_sec == 0 or count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,7 +38,7 @@ window.config(padx=100, pady=75, bg=MAROON) # So that the window doesn't just co
 canvas = Canvas(width=200, height=224, bg=MAROON, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(101, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(101, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
 # Timer Label
@@ -32,7 +46,7 @@ timer_label = Label(text="Timer", font=(FONT_NAME, 32), fg=GREEN, bg=MAROON)
 timer_label.grid(column=1, row=0)
 
 # Buttons
-start_button = Button(text="Start")  # command=start_timer
+start_button = Button(text="Start", command=start_timer)  # command=start_timer
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset")  # command=reset_timer
