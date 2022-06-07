d47e37563a9ac4f5aa003dfa69540c43783fd4cd
CPT <adugyan1602@gmail.com>
2022-06-06 22:59:47 -0500

    UI commit

--- Tkinter/Password_Manager/main.py
+++ Tkinter/Password_Manager/main.py
@@ -1,5 +1,40 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # test
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.config(padx=50, pady=50)  # So that the window doesn't just contain the tomato
+window.title('Password Manager')
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo_img = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+# Website Label & entry
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+# Email/Username Label and entry
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+# Password Label, entry & button
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+# Add button
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
