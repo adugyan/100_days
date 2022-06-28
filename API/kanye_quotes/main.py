1e69ea15e486c74a256eabf02e7fb68055257bbb
adugyan <adugyan1602@gmail.com>
2022-06-27 23:58:44 -0500

    Kayne quote program complete

--- API/kanye_quotes/main.py
+++ API/kanye_quotes/main.py
@@ -1,7 +1,15 @@
 from tkinter import *
-
+import requests
 
 def get_quote():
+    response = requests.get(url='https://api.kanye.rest')
+    response.raise_for_status()
+
+    data = response.json()
+
+    quote = data['quote']
+    canvas.itemconfig(quote_text, text=quote)
+
 
 
 
