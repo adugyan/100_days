2ac0651f613f90f1df946d858153907e52802950
CPT <adugyan1602@gmail.com>
2022-08-26 23:28:53 -0500

    check answer functionality

--- API/Quizzler_App/ui.py
+++ API/Quizzler_App/ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quizbrain: QuizBrain):
+        self.quiz = quizbrain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,6 +18,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some Question Test",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -23,11 +26,25 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.select_true)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.select_false)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def select_true(self):
+        true_option = "True"
+        self.quiz.check_answer(true_option)
+
+    def select_false(self):
+        false_option = "False"
+        self.quiz.check_answer(false_option)
